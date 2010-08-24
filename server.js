82b289cf3c78f6e068e4c9f25d17247bd1df60f8
Ola Karlsson <olkarls@gmail.com>
2010-08-24 21:02:30 +0200

    Server static files.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+require.paths.unshift(__dirname + "/vendor")
+
 var http = require('http'),
-    sys = require('sys');
+    sys = require('sys'),
+    nodeStatic = require('node-static/lib/node-static');
 
 var server = http.createServer(function(request, response) {
+  var file = new nodeStatic.Server('./public', {
+    cache: false
+  });
+  
   request.addListener('end', function() {
-    response.writeHead(200, {
-      'Content-Type': 'text/plain'
-    });
-    response.write(sys.inspect(request));
-    response.end();
+    file.serve(request, response);
   });
 });
 
