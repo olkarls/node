78b1b7c951cc88469262285fef57ae603c76bb74
Ola Karlsson <olkarls@gmail.com>
2010-08-24 21:17:28 +0200

    Structure changes.

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 require.paths.unshift(__dirname + "/vendor")
 
-var http = require('http'),
-    sys = require('sys'),
-    nodeStatic = require('node-static/lib/node-static');
+var LiveStats = require('./lib/livestats');
 
-var server = http.createServer(function(request, response) {
-  var file = new nodeStatic.Server('./public', {
-    cache: false
-  });
-  
-  request.addListener('end', function() {
-    file.serve(request, response);
-  });
+new LiveStats({
+  port: 8000,
+  geoipServer: {
+      hostname: 'geoip.peepcode.com'
+    , port: 80
+  }
 });
 
-server.listen(8000);
