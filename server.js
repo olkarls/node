8a98ff53d929208b6cf201fe45ac57e66267dd8f
Ola Karlsson <olkarls@gmail.com>
2010-08-28 11:35:58 +0200

    Restructure.

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-require.paths.unshift(__dirname + "/vendor")
+require.paths.unshift(__dirname + "/vendor");
+
+process.addListener('uncaughtException', function(err, stack) {
+  console.log('------------------------------------');
+  console.log('Exception: ' + err);
+  console.log(err.stack);
+  console.log('------------------------------------');
+});
 
 var LiveStats = require('./lib/livestats');
 
